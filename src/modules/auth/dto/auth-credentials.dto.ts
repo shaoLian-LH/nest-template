4da648fb1368e647bd2c36dcb67eb3881578b2f2
xue mufan <xuemf@ffcs.cn>
2022-07-25 22:02:07 +0800

    feat: 增加新的校验逻辑和校验提示

--- src/modules/auth/dto/auth-credentials.dto.ts
+++ src/modules/auth/dto/auth-credentials.dto.ts
@@ -1,14 +1,25 @@
 import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { i18nValidationMessage } from 'nestjs-i18n';
 
 export class AuthCredentialsDto {
-  @IsString()
-  @MinLength(6)
+  @IsString({
+    message: i18nValidationMessage('common.TYPE_STRING', {
+      property: 'username',
+    }),
+  })
+  @MinLength(5)
   @MaxLength(20)
   username: string;
 
-  @IsString()
+  @IsString({
+    message: i18nValidationMessage('common.TYPE_STRING', {
+      property: 'password',
+    }),
+  })
   @MinLength(8)
   @MaxLength(32)
-  @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/)
+  @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, {
+    message: i18nValidationMessage('common.AUTH_SIGN_UP'),
+  })
   password: string;
 }
