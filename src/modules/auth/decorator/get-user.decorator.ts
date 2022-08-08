a51cf9ed86e1be624abd79e21c30c8a6ee22a737
xue mufan <xuemf@ffcs.cn>
2022-08-08 22:14:44 +0800

    fix: 删除测试用输出

--- src/modules/auth/decorator/get-user.decorator.ts
+++ src/modules/auth/decorator/get-user.decorator.ts
@@ -4,7 +4,6 @@ import { User } from '../../../entities/user.entity';
 export const GetUser = createParamDecorator(
   (_data, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
-    console.log('req - ', req);
     return req.user;
   },
 );
--- src/modules/cakes/cakes.controller.ts
+++ src/modules/cakes/cakes.controller.ts
@@ -4,8 +4,6 @@ import { PublishCakeDto } from './dto/publish-cake.dto';
 import { Cake } from '../../entities/cake.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiTags } from '@nestjs/swagger';
-import { GetUser } from '../auth/decorator/get-user.decorator';
-import { User } from '../../entities/user.entity';
 
 @ApiTags('蛋糕')
 @Controller('cakes')
@@ -14,11 +12,7 @@ export class CakesController {
   constructor(private readonly cakesService: CakesService) {}
 
   @Post()
-  async publishNewCake(
-    cakeData: PublishCakeDto,
-    @GetUser() user: User,
-  ): Promise<Cake> {
-    console.log('user - ', user);
+  async publishNewCake(cakeData: PublishCakeDto): Promise<Cake> {
     return this.cakesService.publishCakeUnderBrand(cakeData);
   }
 }
